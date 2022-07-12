05d7846d998799cdc314ec7df7c9b293edcdaf45
Raffaello Secchi <r.secchi@gmail.com>
2022-07-12 15:08:45 +0100

    Archive and contacts pages added

--- www/cgi-bin/show_file.py
+++ www/cgi-bin/show_file.py
@@ -33,61 +33,15 @@ else:
 	ff = open(done_page, "r")
 	print(ff.read())
 
+	print('<div class="section">')
+	print('    <h4>Risultati calcolo tecnico</h4>')
+	print('    <p><a href="/output/output.dxf" download>Scarica DXF</a></p>')
+	print('    <p><a href="/output/output.xlsx" download>Scarica XLS</a></p>')
+	print('</div>')
+
 	fin = open(tmp + "output.txt", "r")
 	print(fin.read())
 
-	print("<p>");
-	if "delete" in form.keys():
-		strval = form.getvalue("delete")
-		del_file = tmp + strval[1:-1]
-		if (os.path.exists(del_file)):
-			os.remove(del_file)
-			os.remove(del_file[:-4] + ".txt")
-			os.remove(del_file[:-4] + ".xlsx")
-
-	print("</p>");
-
-	print('<div class="section">')
-	print('<h4>File dispobili sul server</h4>')
-	print('<ul>')
-	floc = os.listdir(tmp)
-	rule = re.compile(".*_leo_.*dxf$")
-	files = list(filter(rule.match, floc))
-	files.sort()
-
-	for f in files:
-		path = tmp + f
-		t = os.path.getmtime(path)
-		href_dxf = '"/output/' + f + '"'
-		href_xls = '"/output/' + f[:-4] + '.xlsx"'
-		href_log = '"/output/' + f[:-4] + '.txt"'
-
-		#rule = re.compile("^.*_leo")
-		#fname = rule.match(f).group(0)
-		#rule = re.search("_leo_.*",f)			
-		#tag = rule.group(0)
-
-		if (len(f) > 30):
-			fname = f[:15]+"..."+f[-15:]
-		else:
-			fname = f
-
-		print('<li>')
-		print('<elem>', fname, '</elem>')
-		#print('<filetag>', tag[5:7], '</filetag>')
-		print('<refs>')
-		print('<ref><a href=',href_dxf,' download>[DXF]</a></ref>')
-		print('<ref><a href=',href_xls,' download>[XLS]</a></ref>')
-		print('<ref><a href=',href_log,' download>[LOG]</a></ref>')
-		print('</refs>')
-		print('<date>', time.ctime(t),'</date>')
-		print('<ref><a href="/cgi-bin/show_file.py?delete=\''+f, end='')
-		print('\'">[x]</a><ref>')
-
-		print('</li>')
-
-	print('</ul>')
-	print('</div>')
 
 	if os.path.exists(logfile):
 		flog = open(logfile, "r")
