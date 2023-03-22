5e48690e353b7bba8f4f7aeac2a782b34446f99e
Raffaello Secchi <r.secchi@gmail.com>
2023-03-22 17:13:29 +0000

    Function cross()

--- engine.c
+++ engine.c
@@ -16,6 +16,35 @@ typedef struct {
 } room;
 
 
+int cross(point* a, point* b)
+{
+
+	double dax = a[1].x - a[0].x;
+	double day = a[1].y - a[0].y;
+	double dbx = b[1].x - b[0].x;
+	double dby = b[1].y - b[0].y;
+
+	double d0x = a[0].x - b[0].x;
+	double d0y = a[0].y - b[0].y;
+
+
+	double dd = -dax * dby + dbx * day;
+	double dt =  d0x * dby - dbx * d0y;
+	double ds = -dax * d0y + d0x * day;
+
+	printf("dd=%lf\n", dd);
+	printf("dd=%lf\n", ds);
+	printf("dd=%lf\n", dt);
+
+	if (dd!=0) {
+		printf("s=%lf\n", ds/dd);
+		printf("t=%lf\n", dt/dd);
+	}
+
+}
+
+
+
 void grid(room r)
 {
 int i,j;
@@ -40,4 +69,15 @@ polygon* obs;
 	}
 
 }
+
+
+
+int main() {
+	printf("HW\n");
+	point l0[2] = {{0,1}, {0,0}};
+	point l1[2] = {{1,1},{1,0}};
+	cross(l0, l1);
+}
+
+
 
