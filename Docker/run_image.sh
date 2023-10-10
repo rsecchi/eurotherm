eec1a0823d6061df4344f11667b0ba42f6993c5d
Raffaello Secchi <r.secchi@gmail.com>
2023-10-10 11:39:59 +0000

    Changes to Docker

--- Docker/run_image.sh
+++ Docker/run_image.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 
-docker run -p 8080:8081 -v $PWD/..:/usr/local/src/eurotherm -v $PWD/../spool:/var/spool/eurotherm eurotherm
+PORT=8080
+ARCHIVE=/data/archive/
 
+
+DOCKER_SPOOL=/var/spool/eurotherm
+DOCKER_SRC=/usr/local/src/eurotherm
+
+
+container_name="devel"
+
+if docker ps -a --format '{{.Names}}' | grep -q "^$container_name$"; then
+	docker stop "$container_name" >/dev/null 2>&1
+	docker rm "$container_name" >/dev/null 2>&1
+fi
+
+docker run -d --name $container_name -p $PORT:8081 -v $PWD/../:$DOCKER_SRC -v $ARCHIVE:$DOCKER_SPOOL eurotherm
+
+#docker run -p $PORT:8081 -v $PWD/../:$DOCKER_SRC eurotherm
