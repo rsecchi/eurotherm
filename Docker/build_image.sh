f0cff6f3a7d358ed2296efa7dd29070626cdad4d
rsecchi <r.secchi@gmail.com>
2023-10-10 23:19:00 +0100

    Docker now creates builds

--- Docker/build_image.sh
+++ Docker/build_image.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-docker build -t eurotherm .
+docker build --network=host -t eurotherm:devel .
+docker build --network=host -t eurotherm:prod -f Dockerfile.prod .
+
+docker login
+docker tag eurotherm:devel raffauser/leonardoplanner:devel
+docker tag eurotherm:prod raffauser/leonardoplanner:prod
+
+docker push raffauser/leonardoplanner:devel
+docker push raffauser/leonardoplanner:prod
+
+
--- Docker/run_image.sh
+++ Docker/run_image.sh
@@ -15,6 +15,6 @@ if docker ps -a --format '{{.Names}}' | grep -q "^$container_name$"; then
 	docker rm "$container_name" >/dev/null 2>&1
 fi
 
-docker run -d --name $container_name -p $PORT:8081 -v $PWD/../:$DOCKER_SRC -v $ARCHIVE:$DOCKER_SPOOL eurotherm
+docker run -d --name $container_name -p $PORT:8081 -v $PWD/../:$DOCKER_SRC -v $ARCHIVE:$DOCKER_SPOOL eurotherm:devel
 
 #docker run -p $PORT:8081 -v $PWD/../:$DOCKER_SRC eurotherm
