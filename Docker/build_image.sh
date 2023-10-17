e6569e53926f832282d191311184e64f5b74810e
Raffaello Secchi <r.secchi@gmail.com>
2023-10-17 21:08:19 +0000

    Changing Docker configuration

--- Docker/build_image.sh
+++ Docker/build_image.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 docker build --network=host -t eurotherm:devel .
-docker build --network=host -t eurotherm:prod -f Dockerfile.prod .
+#docker build --network=host -t eurotherm:prod -f Dockerfile.prod .
 
-docker login
-docker tag eurotherm:devel raffauser/leonardoplanner:devel
-docker tag eurotherm:prod raffauser/leonardoplanner:prod
+#docker login
+#docker tag eurotherm:devel raffauser/leonardoplanner:devel
+#docker tag eurotherm:prod raffauser/leonardoplanner:prod
 
-docker push raffauser/leonardoplanner:devel
-docker push raffauser/leonardoplanner:prod
+#docker push raffauser/leonardoplanner:devel
+#docker push raffauser/leonardoplanner:prod
 
 
