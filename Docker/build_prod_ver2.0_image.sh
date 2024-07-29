abe37b490b8edefeba5b57f1b4e8849a7054e8b5
rsecchi <r.secchi@gmail.com>
2024-07-29 11:09:13 +0100

    Changes to Docker

---
+++ Docker/build_prod_ver2.0_image.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if ! git diff --exit-code; then
+	echo "Please commit and push changes first"
+	exit
+fi
+
+docker build --no-cache --network=host -t raffauser/leonardoplanner:ver2 -f Dockerfile.ver2.0 .
+
+
+#docker login
+#docker tag eurotherm:devel raffauser/leonardoplanner:devel
+#docker tag eurotherm:prod raffauser/leonardoplanner:prod
+
+#docker push raffauser/leonardoplanner:devel
+#docker push raffauser/leonardoplanner:prod
+
+
--- Docker/config.sh
+++ Docker/config.sh
@@ -2,11 +2,11 @@
 
 DIR="/usr/local/src/eurotherm/www"
 	
-cp $DIR/logo-leonardo-planner-1.png $DIR/logo-leonardo-planner.png
+cp $DIR/logo-leonardo-planner-cs-stable.png $DIR/logo-leonardo-planner.png
 
 if [ "${MODE}" == "testing"  ]; then
 	# commands to execute in testing case
-	cp $DIR/logo-leonardo-planner-testing.png $DIR/logo-leonardo-planner.png
+	cp $DIR/logo-leonardo-planner-cs-testing.png $DIR/logo-leonardo-planner.png
 	ARGS="-DTESTING"
 fi
 
