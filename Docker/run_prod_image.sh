dd488ba22d45a89028b3eab9edcb7b585c382362
rsecchi <r.secchi@gmail.com>
2024-07-29 17:19:26 +0100

    Testing modification

--- Docker/run_prod_image.sh
+++ Docker/run_prod_image.sh
@@ -12,14 +12,15 @@ fi
 
 DOCKER_SPOOL=/var/spool/eurotherm
 DOCKER_SRC=/usr/local/src/eurotherm
-DOCKER_IMG=raffauser/leonardoplanner:prod
+DOCKER_IMG=raffauser/leonardoplanner:ver2
 
-container_name="prod"
+container_name="ver2_prod"
 
 if docker ps -a --format '{{.Names}}' | grep -q "^$container_name$"; then
 	docker stop "$container_name" >/dev/null 2>&1
 	docker rm "$container_name" >/dev/null 2>&1
 fi
 
-docker run -d -e MODE=$MODE --name $container_name -p $PORT_PROD:80  -v $ARCHIVE:$DOCKER_SPOOL $DOCKER_IMG
+docker run -d -e MODE=$MODE --name $container_name -p $PORT_PROD:80  \
+	-v $ARCHIVE:$DOCKER_SPOOL $DOCKER_IMG
 
