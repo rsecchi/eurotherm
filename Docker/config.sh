bcbe8a28931672f2a8d4c3ac624ad7288bf5957c
rsecchi <r.secchi@gmail.com>
2025-04-10 19:20:42 +0100

    Adding CS-stable logo to config

--- Docker/config.sh
+++ Docker/config.sh
@@ -8,6 +8,10 @@ if [ "${MODE}" == "testing"  ]; then
 	# commands to execute in testing case
 	cp $DIR/logo-leonardo-planner-cs-testing.png $DIR/logo-leonardo-planner.png
 	ARGS="-DTESTING"
+else
+	# commands to execute in production case
+	cp $DIR/logo-leonardo-planner-cs-stable.png $DIR/logo-leonardo-planner.png
+	ARGS="-DPROD"
 fi
 
 httpd-foreground $ARGS
