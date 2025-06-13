895d6a881064e450f78de4dcaeeffe5350a2675f
Raffaello <raffaello@erg.abdn.ac.uk>
2025-06-13 15:39:42 +0100

    Build libplanner.so from Mac

---
+++ Docker/build_build_image.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+docker build --network=host -t build -f Dockerfile.build .
+#docker build --network=host -t eurotherm:prod -f Dockerfile.prod .
+
+#docker login
+#docker tag eurotherm:devel raffauser/leonardoplanner:devel
+#docker tag eurotherm:prod raffauser/leonardoplanner:prod
+
+#docker push raffauser/leonardoplanner:devel
+#docker push raffauser/leonardoplanner:prod
+
+
